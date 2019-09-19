b496cfda07347d957c128ea752d36984d0684db0
Alex McKirdy <ajmck@users.noreply.github.com>
2019-09-19 18:12:44 +1000

    Client and callback in config.cs file, albeit not reading from user secrets

---
+++ Config.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace splaylist
+{
+    public class Config
+    {
+        private string _clientID = "I've honest to god spent hours trying to get user secrets or environmental variables working for this ID, hence the block being commented out below. If you're unfortunate enough to rip this project off github, replace this variable with a key from https://developer.spotify.com/dashboard/applications/";
+        private string _callbackURI = "http://localhost:51660/callback";
+
+        //private IConfiguration _config;
+
+        //public Config(IConfiguration config)
+        //{
+        //    _config = config;
+
+        //    _clientID = _config["Splaylist:ClientID"];
+        //    _callbackURI = _config["Splaylist:CallbackURI"];
+        //}
+
+
+        public string ClientID { get { return _clientID; } }
+        public string CallbackURI {  get { return _callbackURI; } }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,20 @@
 using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace splaylist
 {
     public class Startup
     {
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<Config>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
+            
             app.AddComponent<App>("app");
         }
     }
