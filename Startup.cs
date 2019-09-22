48371ba930fa5495151a7f8b3745ddab0a06b43c
Alex McKirdy <ajmck@users.noreply.github.com>
2019-09-22 21:28:37 +1000

    Using Blazorise.DataGrid for playlist page

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +19,15 @@ namespace splaylist
             // following shouldn't necessarily be a singleton, but useful for testing / makes it easier in client Blazor
             services.AddSingleton<AuthHelper>();
             services.AddSingleton<APIHelper>();
+
+
+            // Blazorise - used for DataGrid
+            services.AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true; // optional
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
