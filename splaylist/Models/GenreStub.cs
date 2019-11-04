1cfe587a5b2e8186249ae46a97c1fe5a810c11d0
Alex McKirdy <ajmck@users.noreply.github.com>
2019-11-04 13:36:01 +1300

    got genres displaying in a datagrid, and toggling on / off

---
+++ splaylist/Models/GenreStub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace splaylist.Models
+{
+    public class GenreStub
+    {
+        public GenreStub (KeyValuePair<string, Dictionary<string, ListingTrack>> orig)
+        {
+            Genre = orig.Key;
+            Tracks = orig.Value;
+        }  
+
+        public string Genre { get; private set; }
+
+        public Dictionary<string, ListingTrack> Tracks { get; private set; }
+
+        public int Count => Tracks.Count;
+    }
+}
